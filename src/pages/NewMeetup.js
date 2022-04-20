49d89d82d775c7d229c815fcccd205a09a4e88fb
Emmanuel Hateka <emmanuelhateka.com>
2022-04-21 00:11:09 +0100

    partial configuration for sending http request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,19 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch("https://meetup-325d8-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "content-Type": "application/json",
+      },
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddmeetup={addMeetupHandler} />
     </section>
   );
 }

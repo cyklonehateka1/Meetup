2ef7e2abf7f718b577834224f044c81c18d7e703
Emmanuel Hateka <emmanuelhateka.com>
2022-04-23 01:36:43 +0000

    addition and removal of favorites

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const FavoritesContext = createContext({
   favorites: [],
@@ -6,6 +6,29 @@ const FavoritesContext = createContext({
 });
 
 function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+  };
+
   return (
     <FavoritesContext.Provider>{props.children}</FavoritesContext.Provider>
   );
